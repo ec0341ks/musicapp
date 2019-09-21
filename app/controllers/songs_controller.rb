2b7b86915b14eb632e299fb86850150580d7eee8
ec0341ks <with0526@icloud.com>
2019-09-21 13:57:48 +0900

    gem追加/musix_match

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,4 @@
-class SongsController < ApplicationController
+class SongsController < ApplicationControl
+  require 'musix_match'
+  MusixMatch::API::Base.api_key = Rails.application.credentials.musixmatch_secret_key
 end
