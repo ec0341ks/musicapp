803d9c5fe22e094d0325e4b7673c15a920220a27
ec0341ks <with0526@icloud.com>
2019-09-21 14:53:27 +0900

    修正のコミット

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
-class SongsController < ApplicationControl
+class SongsController < ApplicationController
   require 'musix_match'
   MusixMatch::API::Base.api_key = Rails.application.credentials.musixmatch_secret_key
+
 end
