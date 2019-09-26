6693869f5a5852ca7febac3522310e0bfb1d6dbf
ec0341ks <with0526@icloud.com>
2019-09-26 21:08:22 +0900

    検索機能の実装

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,19 @@
 class SongsController < ApplicationController
+
+
+
+
+
   def index
   end
   def search
     artists = RSpotify::Artist.search(input_params)
     @topartists = artists.take(3)
     # binding.pry
-    @keyword = input_params[:name_cont]
   end
   def show
-
   end
-
-
-  
+  private
   def input_params
     params[:name_cont].to_s
   end
