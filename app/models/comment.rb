72204cfb659255cdb9bbe73eedbda2f092289e7d
ec0341ks <with0526@icloud.com>
2019-10-02 19:46:46 +0900

    DBのカラム追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,2 @@
 class Comment < ApplicationRecord
-  create_table :comments do |t|
-    t.string :content
-    t.string :image
-    t.references :user, foreign_key: true
-    t.timestamps
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2019_10_02_104116) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
