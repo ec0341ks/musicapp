2d2ae0c176bb0e161de0240bd0cfb7d3485a983e
ec0341ks <with0526@icloud.com>
2019-10-06 13:25:35 +0900

    修正のコミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,15 @@ class ApplicationController < ActionController::Base
   require 'rspotify'
   RSpotify.authenticate(Rails.application.credentials[:spotify_client_id], Rails.application.credentials[:spotify_client_secret])
 
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # require 'musix_match'
-  # MusixMatch::API::Base.api_key = Rails.application.credentials.musixmatch_secret_key
+  protected
 
-  # require 'rspotify/oauth':
-  # Rails.application.config.middleware.use OmniAuth::Builder do
-  #   provider :spotify, spotify_client_id, spotify_client_secret, scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
 end
