ff8674a97170cca0744a52c1b0f94be122d8d7f5
ec0341ks <with0526@icloud.com>
2019-10-06 17:29:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'songs#index'
-  resources :messages, only: [:index,:create]
   resources :users, only: [:edit, :update]
+    resources :comments, only: [:index,:create]
   resources :songs, only:[:index,:show] do
     collection do
       get 'search'
