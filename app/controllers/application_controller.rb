47931f7a0cbcafd198a278189b4612d9355e4e3b
ec0341ks <with0526@icloud.com>
2019-11-10 18:22:53 +0900

    musixmatchで歌詞表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   require 'rspotify'
   RSpotify.authenticate(Rails.application.credentials[:spotify_client_id], Rails.application.credentials[:spotify_client_secret])
+  require 'musix_match'
+  MusixMatch::API::Base.api_key = Rails.application.credentials[:musixmatch_api_key]
 
   protected
 
